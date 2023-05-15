8fd6ffa756fe23e1be56e0c327231f2d2bb24072
Marcos Junior Oliveira <marcosjoliveiradev@gmail.com>
2023-05-15 11:13:17 -0300

    Exercicio pronto sobre interface

---
+++ ExercicioResolvidoInterface/Services/ITaxService.cs
@@ -0,0 +1,7 @@
+﻿namespace ExercicioResolvidoInterface.Services
+{
+    interface ITaxService
+    {
+        double Tax(double amount);
+    }
+}
---
+++ ExercicioResolvidoInterface/Services/RentalService.cs
@@ -0,0 +1,39 @@
+﻿
+using ExercicioResolvidoInterface.Entities;
+
+namespace ExercicioResolvidoInterface.Services
+{
+    internal class RentalService
+    {
+        public double PricePerHour { get; set; }
+        public double PricePerDay { get; set; }
+        public ITaxService TaxService { get; set; }
+
+        public RentalService(double pricePerHour, double pricePerDay, ITaxService taxService)
+        {
+            PricePerHour = pricePerHour;
+            PricePerDay = pricePerDay;
+            TaxService = taxService;
+        }
+
+        public void ProcessInvoice(CarRental carRental)
+        {
+            TimeSpan duration = carRental.Finish.Subtract(carRental.Start);
+
+            double basicPayment = 0.0;
+            
+            if (duration.TotalHours <= 12.0)
+            {
+                basicPayment = PricePerHour * Math.Ceiling(duration.TotalHours);
+            }
+            else
+            {
+                basicPayment = PricePerDay * Math.Ceiling(duration.TotalDays);
+            }
+
+            double tax = TaxService.Tax(basicPayment);
+
+            carRental.Invoice = new Invoice(basicPayment, tax);
+        }
+    }
+}
